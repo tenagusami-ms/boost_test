af8c9dd1a796af993a454d04e2f4cc70a3595c13
KAN-YA, Yukitoshi <tenagusami@hotmail.com>
2021-11-27 19:41:10 +0900

    coroutines are being tested.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
+#include <iomanip>
+#include <string>
 #include <boost/coroutine2/coroutine.hpp>
 
-using coro_t = boost::coroutines2::coroutine<int>;
+using IntGenerator = boost::coroutines2::coroutine<int>;
+using StringReceiver = boost::coroutines2::coroutine<std::string>;
 
-
+struct FinalEOL{
+    ~FinalEOL(){
+        std::cout << std::endl;
+    }
+};
 
 int main() {
 
-    coro_t::pull_type source(
-            [](coro_t::push_type& sink){
+    IntGenerator::pull_type source(
+            [](IntGenerator::push_type& sink){
                 int first=1,second=1;
                 sink(first);
                 sink(second);
@@ -23,5 +30,34 @@ int main() {
     for(auto i:source) {
         std::cout << i << " ";
     }
+    std::cout<<"\n";
+
+    //const int num=5, width=15;
+    StringReceiver::push_type writer(
+            [num=5, width=15](StringReceiver::pull_type& in){
+                // 何が起きたとしても、最終行を終了する
+                FinalEOL eol;
+                // 上流から値を引き出して、num個を一行に展開する
+                for (;;){
+                    for(int i=0;i<num;++i){
+                        // 入力を使い果たしたらやめる
+                        if(!in) return;
+                        std::cout << std::setw(width) << in.get();
+                        // この要素をハンドルし終わったので、次へ進む
+                        in();
+                    }
+                    // num個の要素の後、改行する
+                    std::cout << std::endl;
+                }
+            });
+
+    std::vector<std::string> words{
+            "peas", "porridge", "hot", "peas",
+            "porridge", "cold", "peas", "porridge",
+            "in", "the", "pot", "nine",
+            "days", "old" };
+
+    std::copy(begin(words),end(words),begin(writer));
+
     return 0;
 }
