ad226efb76352e15eb7d8fafde715b0260043b76
te nagusami <tenagusami@hotmail.com>
2021-12-04 06:43:39 +0900

    module test

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
-#include <iterator>
 #include <boost/coroutine2/coroutine.hpp>
 
 using IntGenerator = boost::coroutines2::coroutine<int>;
 using StringReceiver = boost::coroutines2::coroutine<std::string>;
 
-struct FinalEOL{
-    ~FinalEOL(){
+import moduleTest;
+
+
+struct FinalEOL {
+    ~FinalEOL() {
         std::cout << std::endl;
     }
 };
@@ -20,31 +22,31 @@ int main() {
                 int first=1,second=1;
                 sink(first);
                 sink(second);
-                for(int i=0;i<8;++i){
-                    int third=first+second;
-                    first=second;
-                    second=third;
+                for (int i = 0; i < 8; ++i) {
+                    int third = first + second;
+                    first = second;
+                    second = third;
                     sink(third);
                 }
             })};
 
-    for(auto i:source) {
-        std::cout << i << " ";
+    for (auto number: source) {
+        std::cout << number << " ";
     }
-    std::cout<<"\n";
+    std::cout << "\n";
 
-     auto writer{StringReceiver::push_type(
-            [num=5, width=15](StringReceiver::pull_type& in){
+    auto writer{StringReceiver::push_type(
+            [num = 5, width = 15](StringReceiver::pull_type &inputs) {
                 // 何が起きたとしても、最終行を終了する
                 FinalEOL eol;
                 // 上流から値を引き出して、num個を一行に展開する
-                for (;;){
-                    for(int i=0;i<num;++i){
+                for (;;) {
+                    for (int i = 0; i < num; ++i) {
                         // 入力を使い果たしたらやめる
-                        if(!in) return 1;
-                        std::cout << std::setw(width) << in.get();
+                        if (!inputs) return 1;
+                        std::cout << std::setw(width) << inputs.get();
                         // この要素をハンドルし終わったので、次へ進む
-                        in();
+                        inputs();
                     }
                     // num個の要素の後、改行する
                     std::cout << std::endl;
---
+++ moduleTest.cpp
@@ -0,0 +1,10 @@
+//
+// Created by ykanya on 2021/12/03.
+//
+
+export module
+moduleTest;
+
+export int twice(const int number) {
+    return 2 * number;
+}
